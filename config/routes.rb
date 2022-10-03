cd05829f0ee014f6957f120e5e77ea18fa0db0bc
c011825019 <112187295+c011825019@users.noreply.github.com>
2022-10-03 13:12:50 +0000

    [Add] ユーザー編集機能

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,6 @@ Rails.application.routes.draw do
   get 'homes/about' => 'homes#about', as: 'about'
 
   resources :post_images, only: [:index, :show, :create, :edit, :update, :destroy]
-  resources :users, only: [:show, :edit]
+  resources :users, only: [:show, :edit, :update]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
