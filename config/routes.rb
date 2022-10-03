9f47765cd0a31364235cdae8d5aec8f457286074
c011825019 <112187295+c011825019@users.noreply.github.com>
2022-10-03 12:50:46 +0000

    [Add] Usersコントローラ

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,6 @@ Rails.application.routes.draw do
   get 'homes/about' => 'homes#about', as: 'about'
 
   resources :post_images, only: [:index, :show, :create, :edit, :update, :destroy]
+  resources :users, only: [:show, :edit]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
