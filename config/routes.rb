de0413019015e7271b7120849b51b92bd75e7fa5
c011825019 <112187295+c011825019@users.noreply.github.com>
2022-10-04 06:18:24 +0000

    [Add] コメント投稿機能

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,9 @@ Rails.application.routes.draw do
   root to: 'homes#top'
   get 'homes/about' => 'homes#about', as: 'about'
 
-  resources :post_images, only: [:index, :show, :create, :edit, :update, :destroy]
+  resources :post_images, only: [:index, :show, :create, :edit, :update, :destroy] do
+    resources :post_comments, only: [:create, :destroy]
+  end
   resources :users, only: [:show, :edit, :update]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
