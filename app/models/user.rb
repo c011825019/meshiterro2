d284fb2bdf2b70613f0c245561d5b6827c5c3ec7
c011825019 <112187295+c011825019@users.noreply.github.com>
2022-10-13 15:48:09 +0000

    [Add] フォロー機能

--- app/models/user.rb
+++ app/models/user.rb
@@ -20,4 +20,8 @@ class User < ApplicationRecord
     end
     profile_image.variant(resize_to_limit: [width, height]).processed
   end
+
+  def followed_by?(user)
+    passive_relationships.find_by(following_id: user.id).present?
+  end
 end
